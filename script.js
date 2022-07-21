d79fe5e7b45db2aff1ba74608cc043a869b29e31
Soumyajit Das <mepioneiro@gmail.com>
2022-07-21 15:04:16 +0530

    fix: history management

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const github = "https://github.com/pioneiro/Nagarro/tree/master/";
 
 const pushState = (url) => window.history.pushState({}, "", url);
+const replaceState = (url) => window.history.replaceState({}, "", url);
 
 const main = () => {
   const links = document.querySelectorAll("button[data-link]");
@@ -9,38 +10,50 @@ const main = () => {
   const title = document.getElementById("title");
   const code = document.getElementById("code");
 
+  let active = null;
+
+  const updateWindow = () => {
+    let current = location.hash.slice(1);
+
+    if (!current) {
+      current = "portfolio";
+      replaceState("#portfolio");
+    }
+
+    active?.classList.remove("active");
+
+    active = [...links].filter((link) =>
+      link.dataset.link.includes(current)
+    )[0];
+
+    active.classList.add("active");
+
+    if (!active) {
+      active = links[0];
+      current = "portfolio";
+      replaceState("#portfolio");
+    }
+
+    active.classList.add("active");
+    updateFrame(active);
+  };
+
   const updateFrame = (link) => {
-    frame.src = link.dataset.link;
+    frame.contentWindow.location.replace(link.dataset.link);
     title.innerText = link.innerText;
     document.title = `Nagarro Projects | ${link.innerText}`;
     code.setAttribute("href", new URL(link.dataset.link, github).href);
     loading.style.visibility = "visible";
-    pushState(`#${link.dataset.link.slice(2)}`);
     frame.contentWindow.focus();
   };
 
-  let current = location.hash.slice(1);
+  updateWindow();
 
-  if (!current) {
-    current = "portfolio";
-    pushState("#portfolio");
-  }
-
-  let active = [...links].filter((link) =>
-    link.dataset.link.includes(current)
-  )[0];
-
-  if (!active) {
-    active = links[0];
-    current = "portfolio";
-    pushState("#portfolio");
-  }
-
-  active.classList.add("active");
-  updateFrame(active);
+  window.addEventListener("popstate", updateWindow);
 
   links.forEach((link) => {
     link.addEventListener("click", () => {
+      pushState(`#${link.dataset.link.slice(2)}`);
       updateFrame(link);
 
       active.classList.remove("active");
