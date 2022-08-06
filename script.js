5d056119d9909f0c0889ef565ac5a4299a3888b8
Soumyajit Das <mepioneiro@gmail.com>
2022-08-06 19:28:35 +0530

    modify links

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pushState = (url) => window.history.pushState({}, "", url);
 const replaceState = (url) => window.history.replaceState({}, "", url);
 
 const main = () => {
-  const links = document.querySelectorAll("button[data-link]");
+  const links = document.querySelectorAll("nav > button");
   const loading = document.getElementById("loading");
   const frame = document.getElementById("frame");
   const title = document.getElementById("title");
@@ -23,7 +23,7 @@ const main = () => {
     active?.classList.remove("active");
 
     active = [...links].filter((link) =>
-      link.dataset.link.includes(current)
+      link.dataset.link?.includes(current)
     )[0];
 
     if (!active) {
@@ -50,14 +50,20 @@ const main = () => {
   window.addEventListener("popstate", updateWindow);
 
   links.forEach((link) => {
-    link.addEventListener("click", () => {
-      pushState(`#${link.dataset.link.slice(2)}`);
-      updateFrame(link);
+    "link" in link.dataset &&
+      link.addEventListener("click", () => {
+        pushState(`#${link.dataset.link.slice(2)}`);
+        updateFrame(link);
 
-      active.classList.remove("active");
-      link.classList.add("active");
-      active = link;
-    });
+        active.classList.remove("active");
+        link.classList.add("active");
+        active = link;
+      });
+
+    "github" in link.dataset &&
+      link.addEventListener("click", () => {
+        window.open(new URL(link.dataset.github, github).href, "_blank");
+      });
   });
 
   frame.addEventListener("load", () => {
